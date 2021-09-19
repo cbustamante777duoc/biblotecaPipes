52fc638d6f942ebedf3c99b48f097e9786794a04
cristian <cra.bustamante@alumnos.duoc.cl>
2021-09-19 13:34:16 -0300

    uppercase lowercase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesBibloteca';
+
+  nombre: string = 'Capitan America';
+
+
 }
